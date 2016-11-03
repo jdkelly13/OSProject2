3f2b17b7848d130738bb4121db9bb6a00434779f
jdkelly13 <jeremy.kelly.13@cnu.edu>
2016-11-03 19:35:00 -0400

    Last update for project.

--- Proj2_410_QUEUES_SKELETON/dispatcher/dispatcher.cpp
+++ Proj2_410_QUEUES_SKELETON/dispatcher/dispatcher.cpp
@@ -24,47 +24,59 @@ void dispatcher::init() {
 	runningPCB.start_time = UNINITIALIZED;
 }
 
+//Gets the current job.
 PCB dispatcher::getCurrentJob() {
 	return runningPCB;
 }
 
+//Adds job to the queue.
 void dispatcher::addJob(PCB &myPCB) {
 	ready_Q.push(myPCB);
 }
 
+//Handles process interupts.
 int dispatcher::processInterrupt(int interrupt) {
 	//Checks to see if the interrupt is a time slice interrupt and handles it if it isn't.
-	if(interrupt != TIMESLICE){
+	if (interrupt != SWITCH_PROCESS) {
 		//Checks to see if its an io complete interrupt.
-		if(interrupt == IO_COMPLETE){
+		if (interrupt != IO_COMPLETE) {
 			return PCB_UNIMPLEMENTED;
 		}
 		//Adds elements from the blocked queue to the ready queue.
-		while(!(blocked_Q.empty())){
+		while (!(blocked_Q.empty())) {
 			ready_Q.push(blocked_Q.front());
 			blocked_Q.pop();
 		}
-		
+
 		return PCB_BLOCKED_QUEUE_EMPTY;
 	}
+	else {
+		//Handles if the interrupt is a time slice interrupt.
 
-	//Handles if the interrupt is a time slice interrupt.
+		//Checks to see if the queues are empty.
+		if (ready_Q.empty()) {
+			if (blocked_Q.empty()) {
+				return NO_JOBS;
+			}
+			return BLOCKED_JOBS;
+		}
 
-	//Checks to see if the queues are empty.
-	if(ready_Q.empty() && blocked_Q.empty()){
-		return NO_JOBS;
+
+		//Switches the processes.
+		PCB temp = ready_Q.front();
+		ready_Q.pop();
+		if (runningPCB.process_number != UNINITIALIZED) {
+			ready_Q.push(runningPCB);
+		}
+		runningPCB = temp;
+
+		return PCB_SWITCHED_PROCESSES;
 	}
-
-	//Switches the processes.
-	PCB temp = ready_Q.front();
-	ready_Q.push(runningPCB);
-	runningPCB = temp;
-	
-	return PCB_SWITCHED_PROCESSES;
 }
 
+//Handles the tick of the CPU timer.
 int dispatcher::doTick() {
-
+	std::queue<PCB> blarge = ready_Q;
 	//Checks to see if there is a running PCB and handles if there isnt.
 	if(runningPCB.process_number == UNINITIALIZED){
 
@@ -76,15 +88,20 @@ int dispatcher::doTick() {
 			return BLOCKED_JOBS;
 		}
 		//Handles ready_Q empty being false.
-		runningPCB = getNext();
+		runningPCB = ready_Q.front();
+		ready_Q.pop();
 		return PCB_MOVED_FROM_READY_TO_RUNNING;
 	}
 
 	//Subtracts 1 from the PCB cpu time.
 	(&runningPCB)->cpu_time--;
 
+	if (runningPCB.process_number == 7) {
+		std::cout << "";
+	}
+
 	//Checks to see if the runningPCB is not finished and handles if it isn't.
-	if(runningPCB.cpu_time!=0){
+	if(runningPCB.cpu_time >=1){
 		return PCB_CPUTIME_DECREMENTED;
 	}
 
@@ -93,12 +110,13 @@ int dispatcher::doTick() {
 
 	//Checks to see if the runningPCB makes a blocked IO call and handles it.
 	if(runningPCB.io_time == 1){
+		runningPCB.io_time = 0;
 		blocked_Q.push(runningPCB);
 		returnval = PCB_ADDED_TO_BLOCKED_QUEUE;
 	}
 
 	//Unloads PCB and returns it as finished or invalid based on IO blocking.
-	ready_Q.pop();
+	//ready_Q.pop();
 	runningPCB.cpu_time = UNINITIALIZED;
 	runningPCB.io_time = UNINITIALIZED;
 	runningPCB.process_number = UNINITIALIZED;
